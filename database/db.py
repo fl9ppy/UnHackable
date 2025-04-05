46ab2c1f775a72ccc352b077a3d30b47c45b5092
fl9ppy <adi.calinescu@gmail.com>
2025-04-05 03:50:40 +0300

    BRUHHHH

--- database/db.py
+++ database/db.py
@@ -3,7 +3,7 @@
 import sqlite3
 from pathlib import Path
 
-DB_PATH = Path(__file__).parent / "cyberlingo.db"
+DB_PATH = Path(__file__).parent / "database.db"
 
 def get_connection():
     return sqlite3.connect(DB_PATH)
